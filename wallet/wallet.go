c78857a75225bbfc3f565ba09982c2f0c6af75cc
ChristianLin0420 <crlc112358@gmail.com>
2021-05-03 02:06:49 +0800

    add digital signature

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,6 +1,7 @@
 package wallet
 
 import (
+	"bytes"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
@@ -28,10 +29,6 @@ func (w Wallet) Address() []byte {
 
 	fullHash := append(versionedHash, checksum...)
 	address := Base58Encode(fullHash)
-
-	// fmt.Printf("pub key: %x\n", w.PublicKey)
-	// fmt.Printf("pub hash: %x\n", pubHash)
-	// fmt.Printf("address: %x\n", address)
 
 	return address
 }
@@ -74,4 +71,14 @@ func Checksum(payload []byte) []byte {
 	secondHash := sha256.Sum256(firstHash[:])
 
 	return secondHash[:checksumLength]
+}
+
+func ValidateAddress(address string) bool {
+	pubKeyHash := Base58Decode([]byte(address))
+	actualChecksum := pubKeyHash[len(pubKeyHash)-checksumLength:]
+	version := pubKeyHash[0]
+	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-checksumLength]
+	targetChecksum := Checksum(append([]byte{version}, pubKeyHash...))
+
+	return bytes.Compare(actualChecksum, targetChecksum) == 0
 }
